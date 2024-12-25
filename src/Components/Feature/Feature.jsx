2c7b77016c40772188772f494f38bef36ded709e
Mehefuj Ali <mehefujalim@gmail.com>
2024-12-25 16:28:00 +0530

    feature section card responsive

--- src/Components/Feature/Feature.jsx
+++ src/Components/Feature/Feature.jsx
@@ -9,27 +9,27 @@ const Feature = () => {
                               <p className="w-11/12  md:w-5/12 text-gray-500 dark:text-white">Discover the powerful features of Study Hive that make learning together more effective and enjoyable. From collaborative assignments to real-time feedback, our platform is designed to enhance your study experience.</p>
                         </div>
                         <div className=" mt-20 flex flex-wrap  gap-8 w-11/12 mx-auto justify-center items-center">
-                              <div className=" hover:scale-105 duration-200 ease-in-out text-center  dark:shadow-white shadow-md p-4 rounded-md w-60 h-full flex flex-col justify-between gap-2">
+                              <div className=" hover:scale-105 duration-200 ease-in-out text-center  dark:shadow-white shadow-md p-4 rounded-md w-full sm:w-60 h-full flex flex-col justify-between gap-2">
                                     <img  className=" w-20 mx-auto" src="https://i.ibb.co/QcW31JK/image.png" alt="" />
                                     <h3 className=" text-lg font-bold"> Create and Share Assignments</h3>
                                     <p>Easily create assignments and share them with your friends. Collaborate on group projects and track progress.</p>
                               </div>
-                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-60 h-full flex flex-col justify-between gap-2">
+                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-full sm:w-60 h-full flex flex-col justify-between gap-2">
                                     <img  className=" w-20 mx-auto" src="https://png.pngtree.com/png-vector/20220811/ourmid/pngtree-businessman-hand-shaking-for-the-collaboration-png-image_6104218.png" alt="" />
                                     <h3 className=" text-lg font-bold">Collaborative Learning Environment</h3>
                                     <p>Engage in group study sessions where you and your friends can exchange ideas, solve problems together.</p>
                               </div>
-                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-60 h-full flex flex-col justify-between gap-2">
+                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-full sm:w-60 h-full flex flex-col justify-between gap-2">
                                     <img  className=" w-24 mx-auto object-cover" src="https://i.pinimg.com/originals/72/10/78/721078d6b03e349cea9e59f24b42420c.png" alt="" />
                                     <h3 className=" text-lg font-bold">Grade and Provide Feedback</h3>
                                     <p>Help your peers improve by reviewing and grading their assignments. Provide constructive feedback to encourage growth.</p>
                               </div>
-                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-60 h-full flex flex-col justify-between gap-2">
+                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-full sm:w-60 h-full flex flex-col justify-between gap-2">
                                     <img  className=" w-20 mx-auto object-cover" src="https://cdn-icons-png.flaticon.com/512/8730/8730963.png" alt="" />
                                     <h3 className=" text-lg font-bold"> Secure and Private</h3>
                                     <p>All your data is stored securely, and only registered users can access the assignments and content. Your privacy is our priority.</p>
                               </div>
-                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-60 h-full flex flex-col justify-between gap-2">
+                              <div className="  hover:scale-105 duration-200 ease-in-out  text-center  dark:shadow-white shadow-md p-4 rounded-md w-full sm:w-60 h-full flex flex-col justify-between gap-2">
                                     <img  className=" w-16 mx-auto object-cover" src="https://cdn-icons-png.flaticon.com/512/295/295128.png" alt="" />
                                     <h3 className=" text-lg font-bold">Easy Registration and Login</h3>
                                     <p>Sign up and log in quickly with email, Google, or GitHub. Start your group study sessions with ease!</p>
